397e04590ee9ab224deb1345874a74733fcdeb0d
Umer Abubeker <umer.ali0986@gmail.com>
2025-06-29 17:34:38 -0500

    Intialized ProNet service

---
+++ ProNetService/src/main/java/com/Nextera/ProNetService/ProNetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Nextera.ProNetService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProNetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProNetServiceApplication.class, args);
+	}
+
+}
